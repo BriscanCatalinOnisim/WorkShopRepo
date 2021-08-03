b44c1d03af43591a6acfc966ba80a82c6fce6e75
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-08-03 10:35:13 +0300

    Index js file added to the project

--- HelloWorldWeb/wwwroot/js/site.js
+++ HelloWorldWeb/wwwroot/js/site.js
@@ -3,16 +3,5 @@
 
 // Write your JavaScript code.
 
-// This JS file now uses jQuery. Pls see here: https://jquery.com/
-$(document).ready(function () {
-    // see https://api.jquery.com/click/
-    $("#createButton").click(function () {
-        var newcomerName = $("#nameField").val();
 
-        // Remember string interpolation
-        $("#list").append(`<li>${newcomerName}</li>`);
-
-        $("#nameField").val("");
-    })
-});
 

eae9e3d22b6377a8704c99f0ef3bb81a998fbd03
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-08-12 10:54:25 +0300

    Created the DailyWeather model and WeatherType enum

--- HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloWorldWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,13 @@ namespace HelloWorldWeb.Controllers
     {
         // GET: api/<WeatherController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<DailyWeather> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new DailyWeather[] {
+                new DailyWeather(new DateTime(2021, 8, 12), 23, WeatherType.Mild),
+                new DailyWeather(new DateTime(2021, 8, 12), 23, WeatherType.Mild)
+
+            };
         }
 
         // GET api/<WeatherController>/5
---
+++ HelloWorldWeb/Models/DailyWeather.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldWeb.Models
+{
+    public class DailyWeather
+    {
+        public DailyWeather(DateTime day, float temperature, WeatherType type)
+        {
+            Day = day;
+            Temperature = temperature;
+            Type = type;
+        }
+
+        public float Temperature { get; set; }
+        public WeatherType Type { get; set; }
+        public DateTime Day { get; set; }
+    }
+}

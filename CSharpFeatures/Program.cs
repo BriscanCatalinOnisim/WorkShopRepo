6f4a02faa2c72d897e1f350027d47a85a9bd1047
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-08-19 09:48:04 +0300

    Added CshrapFeatures to the solution

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamMember teamMember = new TeamMember() { Name = "Memeber1" };
+
+            string jsonString = JsonSerializer.Serialize(teamMember);
+
+            Console.WriteLine(jsonString);
+            File.WriteAllText("TeamMember.json", jsonString);
+
+            var teamMemberDeserializer = JsonSerializer.Deserialize<TeamMember>(jsonString);
+
+
+        }
+    }
+}
---
+++ CSharpFeatures/TeamMember.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharpFeatures
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; } 
+    }
+}

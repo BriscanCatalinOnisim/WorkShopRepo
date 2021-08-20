cd0aabcb6bf13d009b8b6762955de4a636333ad1
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-08-20 10:36:37 +0300

    Added signalr to the project. Added chat option to the navbar

--- HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/Controllers/HomeController.cs
@@ -63,5 +63,11 @@ namespace HelloWorldWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
     }
 }
